2ce79af30d24ed4b0076004aeabc368d60eb098f
haritswijaya95 <haritswijaya95@github.com>
2026-04-02 02:58:38 -0700

    fix: postcss config and hydration erro & update file app.tsx for deployment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import "./globals.css"; // Sesuaikan jalur foldernya
 import type { AppProps } from "next/app";
 
 export default function MyApp({ Component, pageProps }: AppProps) {

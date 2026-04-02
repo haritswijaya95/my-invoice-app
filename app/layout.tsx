93b3213077e911196d8ed540b9a1f471325271b1
haritswijaya95 <haritswijaya95@github.com>
2026-04-02 02:03:24 -0700

    fix:update file layout untuk deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./global.css"; // Pastikan nama file sesuai (global.css atau globals.css)
+import '../styles/globals.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
